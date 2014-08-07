082fc3e7539e8a4bff0d04d9113ecfd359251f4a
Manoj Xavier <xpmanoj2@gmail.com>
2014-08-08 04:14:41 +0800

    Added plot1.r and plot.png

---
+++ plot1.R
@@ -0,0 +1,16 @@
+# Download file
+fileURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+temp <- tempfile()
+download.file(fileURL,temp, method ="curl")
+dt <- data.table(read.table(unz(temp, "household_power_consumption.txt"), header = TRUE, sep = ";",na.strings = "?")
+unlink(temp)
+str(dt)
+summary(dt)
+dt_sub = dt[dt$Date == "1/2/2007" | dt$Date == "2/2/2007",]
+nrow(dt_sub)
+
+#Plot on screen first and then save as a PNG
+with(dt_sub, hist(dt_sub$Global_active_power, xlab = "Global Active Power (kilowatts)"))  ## Create plot on screen device
+title(main = "Global Active Power") 
+dev.copy(png, file = "plot1.png")  ## Copy  plot to a PNG file
+dev.off()  
