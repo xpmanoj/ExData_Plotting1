ddad17fe7629f2be09cb342d20ccac70d828077f
Manoj Xavier <xpmanoj2@gmail.com>
2014-08-08 04:27:01 +0800

    Updated plot1.r and plot1.png

--- plot1.R
+++ plot1.R
@@ -4,13 +4,10 @@ temp <- tempfile()
 download.file(fileURL,temp, method ="curl")
 dt <- data.table(read.table(unz(temp, "household_power_consumption.txt"), header = TRUE, sep = ";",na.strings = "?"))
 unlink(temp)
-str(dt)
-summary(dt)
+#Subset for the respective dates
 dt_sub = dt[dt$Date == "1/2/2007" | dt$Date == "2/2/2007",]
-nrow(dt_sub)
-
+nrow(dt_sub) # no of rows after subsetting
 #Plot on screen first and then save as a PNG
 with(dt_sub, hist(as.numeric(as.character(dt_sub$Global_active_power)), xlab = "Global Active Power (kilowatts)", main= "Global Active Power", col ="red"))  ## Create plot on screen device
-
 dev.copy(png, file = "plot1.png")  ## Copy  plot to a PNG file
 dev.off()  
