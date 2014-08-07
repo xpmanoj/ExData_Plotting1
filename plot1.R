fca4099eed7780f3e678359521f7f8fbe3f9b0da
Manoj Xavier <xpmanoj2@gmail.com>
2014-08-08 04:20:20 +0800

    Updated plot1.r

--- plot1.R
+++ plot1.R
@@ -2,7 +2,7 @@
 fileURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
 temp <- tempfile()
 download.file(fileURL,temp, method ="curl")
-dt <- data.table(read.table(unz(temp, "household_power_consumption.txt"), header = TRUE, sep = ";",na.strings = "?")
+dt <- data.table(read.table(unz(temp, "household_power_consumption.txt"), header = TRUE, sep = ";",na.strings = "?"))
 unlink(temp)
 str(dt)
 summary(dt)
@@ -10,7 +10,7 @@ dt_sub = dt[dt$Date == "1/2/2007" | dt$Date == "2/2/2007",]
 nrow(dt_sub)
 
 #Plot on screen first and then save as a PNG
-with(dt_sub, hist(dt_sub$Global_active_power, xlab = "Global Active Power (kilowatts)"))  ## Create plot on screen device
-title(main = "Global Active Power") 
+with(dt_sub, hist(as.numeric(as.character(dt_sub$Global_active_power)), xlab = "Global Active Power (kilowatts)", main= "Global Active Power", col ="red"))  ## Create plot on screen device
+
 dev.copy(png, file = "plot1.png")  ## Copy  plot to a PNG file
 dev.off()  
